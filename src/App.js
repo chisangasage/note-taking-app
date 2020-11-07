3dd20aa8287b17d5d60aee795e9307a4170133c4
chisangasage <chisangasage@gmail.com>
2020-11-07 10:06:18 +0200

    Added routing to the App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
+import Routes from "./Routes"
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Note Taking App</h1>
+    <div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Note</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+      </Navbar>
+      < Routes />
     </div>
   );
 }
 
-export default App;
+export default App ;  
